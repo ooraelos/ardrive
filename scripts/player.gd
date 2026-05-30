e7bd7dd3a337f0a8b903640d95129988e430c6e3
ooraelos <ooraelos@purdue.edu>
2026-05-30 15:05:04 -0400

    movement and idle animations updated

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var last_direction: Vector2 = Vector2.RIGHT
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -12,5 +14,28 @@ func process_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
 	
 	velocity = direction * SPEED
+	
+	process_animation(last_direction)
+
+func process_animation(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", direction)
+	else:
+		play_animation("idle", direction)
+
+func play_animation(prefix: String, dir: Vector2) -> void:
+	
+	if dir.x != 0: #movement towards left or right
+		animated_sprite_2d.flip_h = dir.x < 0 #if going left flip horizontally
+		animated_sprite_2d.play(prefix + "_right")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
