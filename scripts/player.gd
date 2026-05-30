af7289c4865f67d454dd71bfef25821a6eb774ca
ooraelos <ooraelos@purdue.edu>
2026-05-30 14:35:31 -0400

    player sprites and movement added

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+
 func _physics_process(delta: float) -> void:
+	process_movement()
+	move_and_slide()
+
+
+func process_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal := Input.get_axis("ui_left", "ui_right")
-	var vertical := Input.get_axis("ui_up", "ui_down")
-	var direction := Vector2(horizontal, vertical)
+	var direction := Input.get_vector("left", "right", "up", "down")
 	
-	velocity = direction.normalized() * SPEED;
-
-	move_and_slide()
+	velocity = direction * SPEED
