170e5d33f4c6a43b1c45aa3c3b62b25b36e47fef
ooraelos <ooraelos@purdue.edu>
2026-06-02 14:10:03 -0400

    added attack hitbox with rotation

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 var last_direction: Vector2 = Vector2.RIGHT
 var is_attacking: bool = false
+var hitbox_offset: Vector2
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var swing_sword: AudioStreamPlayer2D = $SwingSword
+@onready var hitbox: Area2D = $Hitbox
 
+func _ready() -> void:
+	hitbox_offset = hitbox.position
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("attack") and not is_attacking:
@@ -30,6 +34,7 @@ func process_movement() -> void:
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 		last_direction = direction
+		update_hitbox_offset()
 	else:
 		velocity = Vector2.ZERO
 	
@@ -55,6 +60,8 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 	elif dir.y < 0:
 		animated_sprite_2d.play(prefix + "_up")
 
+# ---ATTACK---
+
 func attack() -> void:
 	is_attacking = true
 	swing_sword.play()
@@ -63,3 +70,19 @@ func attack() -> void:
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if is_attacking:
 		is_attacking = false
+
+# ---HITBOX---
+
+func update_hitbox_offset() -> void:
+	var x := hitbox_offset.x
+	var y := hitbox_offset.y
+	
+	match last_direction:
+		Vector2.LEFT:
+			hitbox.position = Vector2(-x, y)
+		Vector2.RIGHT:
+			hitbox.position = Vector2(x, y)
+		Vector2.UP:
+			hitbox.position = Vector2(y, -x)
+		Vector2.DOWN:
+			hitbox.position = Vector2(-y, x)
